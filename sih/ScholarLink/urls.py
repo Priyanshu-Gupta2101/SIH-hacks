77f34bec4e6d5902eba73346c21374fb5f9d37e0
Somya Barwa <goushala1@gmail.com>
2023-09-07 17:10:42 +0530

    connection++

--- sih/ScholarLink/urls.py
+++ sih/ScholarLink/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name="logout"),
     path('register/', views.register, name="register"),
-
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
 ]
